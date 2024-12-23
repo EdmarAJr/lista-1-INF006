686d8c43d6d6bd52c693b2481fe217893b9b024c
Edmar Amorim <trovador@hotmail.com.br>
2024-12-23 16:13:08 +0000

    CREATE: implementação da função start e insertionSort

--- lista 1/RL1Q1.c
+++ lista 1/RL1Q1.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*definições de tipos e variáveis de pré-processamento*/
-# define lineMaxSize 1000
+# define lineSize 1000
 # define MAX_ListS 50
 # define MAX_NUMBERS 100
 
@@ -20,14 +21,59 @@ void insertionSort(List * input, int listSize);
 
 
 int main() {
-    int start();
-
+    start();
     return 0;
 }
 
 int start(){
-    FILE *input = "L1Q1.in";
-    FILE *output = "L1Q1.out";
+    FILE *input = fopen("L1Q1.in", "r");
+    FILE *output = fopen("L1Q1.out", "w");
+	char line[lineSize];
+	char split[] = " ";
+	char *outer;
+	/*contador dos itens da lista*/
+	int count = 0;
+	
+	if (!input || !output) {
+        printf("Erro ao abrir os arquivos.\n");
+        exit;
+    }
+
+	/*copia cada linha do input no comprimento de 1000 caracteres para line*/
+	while(fgets(line, lineSize, input) != NULL){ /*enquanto existir carater válido na linha*/
+		/*cópia auxiliar de cada linha*/
+		char *token = strtok_r(line, split, &outer);
+		
+		/*nova lista sem o start*/
+		List lineList[MAX_ListS] = {0};
+
+		while (token != NULL) {
+			if (strcmp(token, "start") == 0) {
+        		token = strtok_r(NULL, " ", &outer);
+				while(strcmp(token, "start") != 0) { 
+				printf("%s\n", token);
+                	lineList[count].numberList[lineList[count].index] = atoi(token);
+					lineList[count].sum += lineList[count].numberList[lineList[count].index] ;
+					lineList[count].index ++;
+					token = strtok_r(NULL, split, &outer);
+					if(token == NULL) break;
+            	}
+        		count++;
+        	}	
+		}
+		// if(fgets(line, lineSize, input) != NULL) {
+		// 	save(lineList, count, output, 0);
+		// }
+		// else {
+		// 	save(lineList, count, output, 1);
+		// 	break;
+		// }
+		count = 0;
+	}
+
+	fclose(input);
+	fclose(output);
+	EXIT_SUCCESS;
 }
 
 void insertionSort(List * input, int listSize) {
