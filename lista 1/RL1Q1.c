3ff0cb15ac35533218f809c27347bc497a5b7e07
Edmar Amorim <trovador@hotmail.com.br>
2024-12-24 03:36:01 +0000

    FIX: correção do comportamento do fgets dentro do primeiro laço.

--- lista 1/RL1Q1.c
+++ lista 1/RL1Q1.c
@@ -8,17 +8,15 @@
 # define MAX_NUMBERS 100
 
 typedef struct list {
-	int sum;
-	int index;
 	int numberList[MAX_NUMBERS];
+	int itemCount;
+	int sum;/*serve para verificar quem tem o maior somatório e assim organiza as células*/
 } List;
-
-
 
 /*protótipos de funções*/
 int start();
-void insertionSort(List * input, int listSize);
-
+void save(List * input, int listSize, FILE * fileOut, int isLast);
+void insertionSortCells(List * input, int listSize);
 
 int main() {
     start();
@@ -28,63 +26,79 @@ int main() {
 int start(){
     FILE *input = fopen("L1Q1.in", "r");
     FILE *output = fopen("L1Q1.out", "w");
-	char line[lineSize];
-	char split[] = " ";
-	char *outer;
-	/*contador dos itens da lista*/
-	int count = 0;
 	
 	if (!input || !output) {
-        printf("Erro ao abrir os arquivos.\n");
-        exit;
+        printf("Error opening files.\n");
+        return EXIT_FAILURE;
     }
+	/*cria uma nova linha*/	
+	char * line = malloc(lineSize * sizeof(char));
+	/*separador dos itens da linha*/
+	char split[] = " ";
+	/*ponteiro para salvar os dados separados da função strtok_r (thread-safe)*/
+	char *outer;
+	/*contador dos itens da lista*/
+	int cellCounter = 0;
+	
 
 	/*copia cada linha do input no comprimento de 1000 caracteres para line*/
-	while(fgets(line, lineSize, input) != NULL){ /*enquanto existir carater válido na linha*/
+	fgets(line, lineSize, input);
+	/*percorre a linha enquanto existir caractere válido*/
+	while(line != NULL){ 
 		/*cópia auxiliar de cada linha*/
 		char *token = strtok_r(line, split, &outer);
 		
 		/*nova lista sem o start*/
-		List lineList[MAX_ListS] = {0};
-
+		List currentLine[MAX_ListS] = {0};
+		/*percorre o dado enquanto existir caractere válido*/
 		while (token != NULL) {
-			if (strcmp(token, "start") == 0) {
-        		token = strtok_r(NULL, " ", &outer);
+			/*remove o start de cada novo dado. Ex: 3 -1 2 0 start 4 2 5 1 0 start 2 1 -3 start 2 1 100 -3 start 2 101 -3*/
+			if (strcmp(token, "start") == 0) { 
+				/*copia o dado para o ponteiro outer. thread-safe permite a tokenização de múltiplas strings simultaneamente sem interferência entre elas.*/
+        		token = strtok_r(NULL, " ", &outer); 
+				/*percorre o dado sem start enquanto for diferente de start Ex: 3 -1 2 0 start*/
 				while(strcmp(token, "start") != 0) { 
-				printf("%s\n", token);
-                	lineList[count].numberList[lineList[count].index] = atoi(token);
-					lineList[count].sum += lineList[count].numberList[lineList[count].index] ;
-					lineList[count].index ++;
-					token = strtok_r(NULL, split, &outer);
-					if(token == NULL) break;
+					/*insere os dados de token na linha atual[contador de células].lista de números[linha atual[contador de células].contagem de itens]*/
+                	currentLine[cellCounter].numberList[currentLine[cellCounter].itemCount] = atoi(token);
+					//salva a soma para organizar a células
+					currentLine[cellCounter].sum += currentLine[cellCounter].numberList[currentLine[cellCounter].itemCount];
+					/*atualiza o valor do contador de itens armazenadas em cada célula*/
+					currentLine[cellCounter].itemCount++; 
+					/*atualiza o valor do token para inicar novamente*/
+					token = strtok_r(NULL, split, &outer); 
+					/*caso chegue ao final da linha ou encontre um valor nulo, interrompe o loop*/
+					if(token == NULL) break; 
             	}
-        		count++;
+				/*atualiza o valor do contado de células e vai para a próxima célula na linha*/
+        		cellCounter++; 
         	}	
 		}
-		// if(fgets(line, lineSize, input) != NULL) {
-		// 	save(lineList, count, output, 0);
-		// }
+		
+		// if(fgets(line, lineSize, input) != NULL) 
+		// 	save(currentLine, cellCounter, output, 0);
 		// else {
-		// 	save(lineList, count, output, 1);
+		// 	save(currentLine, cellCounter, output, 1);
 		// 	break;
 		// }
-		count = 0;
+		cellCounter = 0;
 	}
 
+	free(line);
 	fclose(input);
 	fclose(output);
 	EXIT_SUCCESS;
 }
 
-void insertionSort(List * input, int listSize) {
-	for (int i = 1; i < listSize; i++) {
-		List key = input[i];
+/*ordena os itens das células*/
+void insertionSortItems(List * input) {
+	for (int i = 1; i < input->itemCount; i++) {
+		int key = input->numberList[i];
 		int j = i - 1;
-
-		while (j >= 0 && key.sum < input[j].sum) {
-			input[j + 1] = input[j];
+		while (j >= 0 && key < input->numberList[j]) {
+			input->numberList[j + 1] = input->numberList[j];
 			j--;
 		}
-		input[j + 1] = key;
+		input->numberList[j + 1] = key;
 	}
 }
+
